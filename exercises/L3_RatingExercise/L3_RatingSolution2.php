cac5b607d25bcdd2141898eb8ce74d39ee2a1b23
Kim Rowan <kim.rowan@cancer.org.uk>
2012-07-12 13:01:50 +0100

    Remove "bad comments" from rating exercise solution files

--- exercises/L3_RatingExercise/L3_RatingSolution2.php
+++ exercises/L3_RatingExercise/L3_RatingSolution2.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * A refactored version of solution 1 that generates a random 5 star 
- * rating as a decimal number. 
+ * A refactored version of solution 1 that generates a random 5 star
+ * rating as a decimal number.
  *
- * As well as the code being refactored, this solution outputs the 
+ * As well as the code being refactored, this solution outputs the
  * result as <img> tags using the star images in the assets directory
  *
  */
@@ -12,41 +12,35 @@
 function setRating()
 {
     //generate a random whole or decimal number between 0 and 5
-    //retain original value for display purposes
     $rating = rand(0,50) / 10;
 
-    //initialise partialStar variable to false
     $partialStar = 0;
 
-    //round rating down nearest whole number
+    //round rating down to nearest whole number
     $fullStars = floor($rating);
 
     //determine whether a partial star is required
     if ($rating - $fullStars > 0) $partialStar = 1;
 
-    //calculate number of empty stars required to display
+    //calculate number of empty stars required
     $emptyStars = 5 - $fullStars - $partialStar;
 
-    //initialise variable to hold star rating output
     $ratingDisplay = '';
 
-    //generate html for full stars, if required
+    //generate html output
     for($i = 0; $i < $fullStars; $i++) {
         $ratingDisplay .= '<img src="assets/star_full.png" alt="Star image"/>';
     }
 
-    //generate html for partial star, if required
     if ($partialStar)
     {
         $ratingDisplay .= '<img src="assets/star_partial.png" alt="Star image"/>';
     }
 
-    //generate html for empty stars, if required
     for($i = 0; $i < $emptyStars; $i++) {
         $ratingDisplay .= '<img src="assets/star_empty.png" alt="Star image"/>';
     }
 
-    //print star rating as html, with actual rating value in parenthesis
     echo $ratingDisplay . '  (' . $rating . ")";
 }
 
